e74119faf3fafb91a25c16c7c8587b8f84276a69
Ajit <ajiteshgusain@gmail.com>
2026-05-01 21:58:34 +0530

    Refactor code structure for improved readability and maintainability

---
+++ jurrassic-dasboard/src/App.jsx
@@ -0,0 +1,14 @@
+function App(){
+  return(
+    <div className="flex place-items-center justify-center  h-screen bg-black">
+
+      <h1 className="text-5xl text-green-400 font-bold">
+        🦖 Jurassic Park Dashboard
+      </h1>
+
+    </div>
+  );
+
+}
+
+export default App;
